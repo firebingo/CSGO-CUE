4da35b7d2b1e3c2dfd79618c10e8cd8964af22f2
Matthew <matthew.t.kides@cox.net>
2016-07-28 19:51:54 -0700

    Another minor fix for timing differences related to thread sleep depending how the program was ran.

--- csgo_k70/Program.cs
+++ csgo_k70/Program.cs
@@ -34,8 +34,12 @@ namespace CSGO_K70
 				Thread.Sleep(4);
                 Core.mainUpdate(dt);
                 timer.Stop();
-                dt = Convert.ToSingle((timer.Elapsed.TotalMilliseconds + 4) / 1000);
-            }
+#if DEBUG
+				dt = Convert.ToSingle((timer.Elapsed.TotalMilliseconds + 4) / 1000);
+#else
+				dt = Convert.ToSingle((timer.Elapsed.TotalMilliseconds) / 1000);
+#endif
+			}
             while (Core.isRunning);
 
             //kill the input thread before the application closes.
